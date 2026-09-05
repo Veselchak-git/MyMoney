42102b75a16fc4a09271f9c119f57dddbe7907b7
Artem Galuzin <galuzinartem123@gmail.com>
2026-09-05 16:12:55 +0300

    release 1.0.3

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ru.veselchak.mymoney',
   appName: 'Мои Финансы',
-  version: '1.0.0',
+  version: '1.0.3',
   webDir: 'dist/MyMoney/browser',
   server: {
     androidScheme: 'https',
@@ -12,7 +12,7 @@ const config: CapacitorConfig = {
     SplashScreen: {
       launchShowDuration: 2000,
       launchAutoHide: true,
-      backgroundColor: '#FFFFFF',
+      backgroundColor: '#10B981',
       androidSplashResourceName: 'splash',
       androidScaleType: 'CENTER_INSIDE',
       showSpinner: false,
